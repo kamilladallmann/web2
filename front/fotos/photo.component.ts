d8fa11e83d167ea1eab3922dbad750e749d8d27f
kamilladallmann <kamilladallmann@hotmail.com>
2018-10-16 17:16:51 -0300

    metodos para subir fotos

---
+++ front/fotos/photo.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { AuthService } from '../services/auth.service';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent implements OnInit {
+
+  constructor(private api: ApiService, 
+              private user: AuthService,
+              private snackEmail: MatSnackBar,
+              private router: Router) { }
+  public usuario: any;
+  public id: String
+  public namePhoto = [];
+  public imagens = [];
+  public photos: string[] = [];
+  ngOnInit() {
+    this.usuario = localStorage.getItem('id');
+    this.id = localStorage.getItem('id');
+  }
+  form = new FormGroup({
+    nome: new FormControl('', [Validators.required]),
+    photos: new FormControl()
+  })
+
+
+  foto(e) {
+    // const reader = new FileReader();
+    
+    for (let i = 0; i < e.target.files.length; i++) {
+
+      this.salvarFotos(e.target.files[i], i);
+      this.photos.push(e.target.files[i].name);
+    }
+
+    // let result: string;
+    // reader.onload = (img) => {
+    //   this.imagens = reader.result;
+    // };
+    // this.namePhoto = rawImg.name;
+    // reader.readAsDataURL(rawImg);
+
+
+  }
+  salvarFotos(rawImg: File, i) {
+    let result: string;
+    const reader = new FileReader();
+    reader.onload = (img) => {
+      this.imagens.push(reader.result);
+    };
+    this.namePhoto.push(rawImg.name);
+    reader.readAsDataURL(rawImg);
+  }
+  onSalvarPhotos(e) {
+    e.preventDefault();
+    var obj = {
+      nome: this.form.get('nome').value,
+      photo: this.imagens,
+      userID: this.id ,
+      namePhoto: this.namePhoto
+    }
+    this.api.salvarFotos(obj).subscribe(res => {
+      this.openSnackBar("fotos salvas com sucesso");
+      this.router.navigate(['/dashboard']);
+    }, err => {
+      
+      var tmp = JSON.parse(err._body);
+      console.log(tmp);
+      this.openSnackBar(tmp.message);
+    });
+
+  };
+  openSnackBar(frase) {
+    this.snackEmail.open(frase, 'OK', {
+      duration: 6000
+    });
+  }
+  verAlbuns()
+  {
+    this.router.navigate(['/dashboard']);
+  }
+  popPhoto(photo: string) {
+    console.log(photo);
+    console.log(this.namePhoto);
+    this.photos = this.photos.filter(photoitem => photoitem !== photo);
+    const pos = this.namePhoto.indexOf(photo);
+    console.log(pos);
+    this.namePhoto = this.namePhoto.filter(np => np != photo);
+    console.log(this.namePhoto);
+    console.log(this.imagens);
+    this.imagens = this.imagens.filter((element, i) =>  i == pos);
+    console.log(this.imagens);
+  }
+}
